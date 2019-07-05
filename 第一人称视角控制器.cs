9ee4e3ed921316a294ae433a2d2cdef64a8982ca
Fang <2968697144@qq.com>
2019-07-05 20:08:13 +0800

    Add files via upload
    第一人称视角控制器

---
+++ 第一人称视角控制器.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///<summary>
+///第一人称视角
+///</summary>
+public class FPV : MonoBehaviour 
+{
+    //鼠标灵敏度
+    public float sensitivityX = 10f;
+    public float sensitivityY = 8f;
+    //最大上下视角
+    public float maximumY = 60f;
+    public float minimumY = -60f;
+    //相机实际旋转量
+    float rotationY = 0f;
+    float rotationX;
+    Vector3 rotation;
+
+    private void Update()
+    {
+        rotationX =
+            Camera.main.transform.localEulerAngles.y + 
+            Input.GetAxis("MouseX") * sensitivityX;
+        rotationY += Input.GetAxis("MouseY") * sensitivityY;
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+        rotation.y = rotationX;
+        rotation.x = -rotationY;
+        transform.localEulerAngles = rotation;
+    }
+}
