ec08be078b83fd3ebb166c902e44f31fdfe06a5b
Fang <2968697144@qq.com>
2019-07-06 16:03:15 +0800

    关于射线得小应用

---
+++ 射线简单应用.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///<summary>
+///射线
+///V1.0 射线击中碰撞体，画出线
+///V2.0 实现点击开关门
+///</summary>
+public class RayTest : MonoBehaviour 
+{
+    private RaycastHit hitInfo;
+    private Vector3 hitPoint;
+
+
+    //V1.0
+    //private void Update()
+    //{
+    //    if (Input.GetMouseButton(0))
+    //    {
+    //        Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //        if (Physics.Raycast(r, out hitInfo) == true)
+    //        {
+    //            Debug.DrawLine(Camera.main.transform.position, hitInfo.point,Color.red);
+    //        }
+    //    }
+    //}
+
+    private Door door;
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(r, out hitInfo)&&hitInfo.transform.tag == "door" )
+            {
+                door = hitInfo.transform.GetComponent<Door>();
+                if (door.ds == DoorState.closed)
+                {
+                    door.Open();
+                }
+                else
+                {
+                    door.Close();
+                }
+            }
+        }
+    }
+}
