3455f57b8c694309558c1ffe24467e92fc23fa2d
Fang <2968697144@qq.com>
2019-07-06 16:03:56 +0800

    门仿真
    门仿真

---
+++ 门/Door.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///<summary>
+///门
+///</summary>
+public class Door : MonoBehaviour 
+{
+    [HideInInspector]
+    public DoorState ds;
+
+    private void Start()
+    {
+        this.Close();//初始化门状态
+    }
+
+    public void Open()
+    {
+        if (ds == DoorState.closed)
+        {
+            //开门操作
+            ds = DoorState.open;
+            transform.rotation = 
+                transform.parent.rotation*Quaternion.Euler(0,90,0);       
+        }
+    }
+
+    public void Close()
+    {
+        if (ds == DoorState.open)
+        {
+            //关门操作
+            ds = DoorState.closed;
+            transform.rotation = transform.parent.rotation;
+        }
+    }
+
+}
---
+++ 门/DoorState.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+///<summary>
+///DoorState
+///</summary>
+public enum DoorState  
+
+{ open, closed };
+
