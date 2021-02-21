dd274fee636a932ede1723fc029091d4a7eea156
Fang <2968697144@qq.com>
2021-02-21 18:08:59 +0800

    asset序列化
    asset序列化，将C#转化为.asset文件，并读取.asset文件

---
+++ AssetSerializeExample.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetSerializeExample : MonoBehaviour
+{
+    void ReadAsset()
+    {
+        AssetSeriTest ast = UnityEditor.AssetDatabase.LoadAssetAtPath<AssetSeriTest>("Assets/TestAsset.asset");
+        if (ast == null)
+        {
+            Debug.LogError("AssetSeriTest ast is not exist");
+        }
+        Debug.Log(ast.Id);
+        Debug.Log(ast.name);
+        foreach (int item in ast.List)
+        {
+            Debug.Log(item);
+        }
+    }
+
+    private void Start()
+    {
+        ReadAsset();
+    }
+}
+
+[CreateAssetMenu(fileName ="TestAsset",menuName ="CreateTestAsset",order =0)]
+public class AssetSeriTest : ScriptableObject
+{
+    public int Id;
+    public string Name;
+    public List<int> List = new List<int>();
+}
